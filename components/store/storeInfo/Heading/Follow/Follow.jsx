cc4a8fcb9ef8917ca65178fed9d38a45fe6b7c59
kyleowen12345 <gakyleowen@gmail.com>
2021-06-30 10:01:57 +0800

    follow func test

--- components/store/storeInfo/Heading/Follow/Follow.jsx
+++ components/store/storeInfo/Heading/Follow/Follow.jsx
@@ -4,6 +4,8 @@ import { Box,Button,Icon } from "@chakra-ui/react"
 import {  RiUserFollowLine,RiUserUnfollowLine} from "react-icons/ri"
 import { useAuth } from '../../../../../lib/auth';
 import { useRouter } from 'next/router'
+import { STORESINFO } from '../../../../../pages/stores/info/[id]';
+
 const FOLLOWSTORE = gql`
 mutation ($id:ID!,$storeName:String!,$storeType:String!,$storeBackgroundImage:String!){
     followStore(id:$id,storeName:$storeName,storeType:$storeType,storeBackgroundImage:$storeBackgroundImage){
@@ -23,13 +25,14 @@ mutation ($id:ID!){
 const Follow = ({store,follower}) => {
     const {authToken}=useAuth()
     const router = useRouter()
-    const [followstore] = useMutation(FOLLOWSTORE,{ errorPolicy: 'all' });
-    const [unfollowstore] = useMutation(UNFOLLOWSTORE,{ errorPolicy: 'all' });
+    const {id}= router.query
+    const [followstore,{loading:followLoad,error:followError}] = useMutation(FOLLOWSTORE,{ errorPolicy: 'all' });
+    const [unfollowstore,{loading:UnfollowLoad,error:UnfollowError}] = useMutation(UNFOLLOWSTORE,{ errorPolicy: 'all'});
     const onFollow = async() => {
         if(!authToken){
             return  router.push("/login")
         } 
-        const{data}=await  followstore({variables:{id:store.id,storeName:store.storeName,storeType:store.storeType,storeBackgroundImage:store.storeBackgroundImage},context:{headers:{token:authToken || ""}}})
+        const{data}=await  followstore({variables:{id:store.id,storeName:store.storeName,storeType:store.storeType,storeBackgroundImage:store.storeBackgroundImage},context:{headers:{token:authToken || ""}},refetchQueries:[{query:STORESINFO,variables:{id:id },context:{headers:{token:authToken || ""}}}]})
         if(data){
          console.log(data)
         }
@@ -38,19 +41,20 @@ const Follow = ({store,follower}) => {
         if(!authToken){
            return  router.push("/login")
           } 
-        const{data}=await  unfollowstore({variables:{id:store.id},context:{headers:{token:authToken || ""}}})
-         if(data){
+        const{data}=await  unfollowstore({variables:{id:store.id},context:{headers:{token:authToken || ""}},refetchQueries:[{query:STORESINFO,variables:{id:id },context:{headers:{token:authToken || ""}}}]})
+        
+        if(data){
            console.log(data)
          }
 };
-console.log(follower)
+console.log(followLoad)
     return (
         <Box pr=".625rem" flex={1} >
             {
              follower == true ? 
-             <Button bg="transparent" fontSize="14px" borderRadius="none" color="white" border="1px solid white" width="100%" _hover={{bg:"transparent"}} onClick={onUnFollow}><Icon as={RiUserUnfollowLine} mr={3} /> Unfollow</Button>
+             <Button bg="transparent" fontSize="14px" borderRadius="none" color="white" border="1px solid white" width="100%" _hover={{bg:"transparent"}} onClick={onUnFollow} isLoading={followLoad}><Icon as={RiUserUnfollowLine} mr={3} /> Unfollow</Button>
              :
-             <Button bg="transparent" fontSize="14px" borderRadius="none" color="white" border="1px solid white" width="100%" _hover={{bg:"transparent"}} onClick={onFollow}><Icon as={RiUserFollowLine} mr={3} /> Follow</Button>   
+             <Button bg="transparent" fontSize="14px" borderRadius="none" color="white" border="1px solid white" width="100%" _hover={{bg:"transparent"}} onClick={onFollow} isLoading={UnfollowLoad}><Icon as={RiUserFollowLine} mr={3} /> Follow</Button>   
             }
             
         </Box>
