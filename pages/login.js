e8912896a509ebdba3fac28b84c8f3df78c657cd
kyleowen12345 <gakyleowen@gmail.com>
2021-07-14 12:59:47 +0800

    auth fix 2

--- pages/login.js
+++ pages/login.js
@@ -33,7 +33,7 @@ export default function Login() {
             const{data}=await  login({variables:{email:email,password:password}})
             if(data){
              Login(data?.login.token)
-             router.push("/")
+             router.push("/stores/1?sortOrder=storeName")
             }
     };
     return (
